2433cc12861d62828a086bbe81df5213d2b4f9ed
esc358 <200478144@student.georgianc.on.ca>
2025-07-06 15:35:03 -0400

    completed logic for NpLinearAlgebra.py, passed all tests in HackerRank

--- HackerRank/LinearAlgebra/NpLinearAlgebra.py
+++ HackerRank/LinearAlgebra/NpLinearAlgebra.py
@@ -2,5 +2,36 @@
 #July 6, 2025
 #From: https://www.hackerrank.com/challenges/merge-the-tools/problem?isFullScreen=true
 
+#install package as venv
+#import Numpy Library
+import numpy as np
 
+#cast int input, is the size of matrix i.e 2 for 2x2
+numberInput = int(input())
 
+#initialize a matrix of lis
+matrix = []
+
+#for loop for range of numberInput
+for i in range(numberInput):
+    #take input from user as string
+    rowInput = input()
+    #split string into a list
+    rowValues = rowInput.split()
+    #create empyt list of rows
+    rowFloatsList = []
+    #for loop row values
+    for value in rowValues:
+        #cast string to float
+        floatValue = float(value)
+        #append to rowFloats list
+        rowFloatsList.append(floatValue)
+
+    #append to matrix the rowFloatsMatrix
+    matrix.append(rowFloatsList)
+
+#use Numpy method
+determinant = np.linalg.det(matrix)
+
+#round to 2 decimals and print
+print(round(determinant, 2))
